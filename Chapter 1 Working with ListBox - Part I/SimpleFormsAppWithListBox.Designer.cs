47a23a8931cdeca13f0a47aca434d7e7b968b2f1
Gian Kyrle Tradio <94472745+GianKyrleTradio@users.noreply.github.com>
2025-03-10 10:21:53 +0800

    Add files via upload

---
+++ Chapter 1 Working with ListBox - Part I/SimpleFormsAppWithListBox.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace SimpleFormsAppWithListBox
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Add = new System.Windows.Forms.Button();
+            this.lstBox = new System.Windows.Forms.ListBox();
+            this.txtBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Delete = new System.Windows.Forms.Button();
+            this.Clear = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Add
+            // 
+            this.Add.Font = new System.Drawing.Font("Modern No. 20", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Add.Location = new System.Drawing.Point(12, 110);
+            this.Add.Name = "Add";
+            this.Add.Size = new System.Drawing.Size(128, 42);
+            this.Add.TabIndex = 0;
+            this.Add.Text = "Add";
+            this.Add.UseVisualStyleBackColor = true;
+            this.Add.Click += new System.EventHandler(this.AddButton_Click);
+            // 
+            // lstBox
+            // 
+            this.lstBox.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstBox.FormattingEnabled = true;
+            this.lstBox.ItemHeight = 18;
+            this.lstBox.Location = new System.Drawing.Point(12, 158);
+            this.lstBox.Name = "lstBox";
+            this.lstBox.Size = new System.Drawing.Size(417, 382);
+            this.lstBox.TabIndex = 1;
+            this.lstBox.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // txtBox
+            // 
+            this.txtBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtBox.Location = new System.Drawing.Point(157, 75);
+            this.txtBox.Name = "txtBox";
+            this.txtBox.Size = new System.Drawing.Size(128, 20);
+            this.txtBox.TabIndex = 2;
+            this.txtBox.TextChanged += new System.EventHandler(this.txtBox_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Modern No. 20", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(90, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(266, 29);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Simple Name Viewer";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Delete
+            // 
+            this.Delete.Font = new System.Drawing.Font("Modern No. 20", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Delete.Location = new System.Drawing.Point(157, 110);
+            this.Delete.Name = "Delete";
+            this.Delete.Size = new System.Drawing.Size(128, 42);
+            this.Delete.TabIndex = 4;
+            this.Delete.Text = "Delete";
+            this.Delete.UseVisualStyleBackColor = true;
+            this.Delete.Click += new System.EventHandler(this.Delete_Click);
+            // 
+            // Clear
+            // 
+            this.Clear.Font = new System.Drawing.Font("Modern No. 20", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Clear.Location = new System.Drawing.Point(301, 110);
+            this.Clear.Name = "Clear";
+            this.Clear.Size = new System.Drawing.Size(128, 42);
+            this.Clear.TabIndex = 5;
+            this.Clear.Text = "Clear All Items";
+            this.Clear.UseVisualStyleBackColor = true;
+            this.Clear.Click += new System.EventHandler(this.Clear_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(73, 75);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 15);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Input Here:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(441, 555);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Clear);
+            this.Controls.Add(this.Delete);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtBox);
+            this.Controls.Add(this.lstBox);
+            this.Controls.Add(this.Add);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Add;
+        private System.Windows.Forms.ListBox lstBox;
+        private System.Windows.Forms.TextBox txtBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button Delete;
+        private System.Windows.Forms.Button Clear;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Chapter 1 Working with ListBox - Part I/SimpleFormsAppWithListBox.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SimpleFormsAppWithListBox
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void AddButton_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(txtBox.Text) && !lstBox.Items.Contains(txtBox.Text)) 
+            {
+
+                lstBox.Items.Add(txtBox.Text);    
+
+            }
+
+        }
+
+        private void Delete_Click(object sender, EventArgs e)
+        {
+
+            lstBox.Items.Remove(lstBox.SelectedItem);
+
+        }
+
+        private void Clear_Click(object sender, EventArgs e)
+        {
+
+            lstBox.Items.Clear();
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
