e79f95371a5a9ec6eca64fd058d3b90bd061d35e
Gian Kyrle Tradio <94472745+GianKyrleTradio@users.noreply.github.com>
2025-03-14 11:37:33 +0800

    Add files via upload

---
+++ Chapter 1 Working with CheckedListBox - Part I/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace SimpleFormsAppWithCheckedListBox
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Add = new System.Windows.Forms.Button();
+            this.Remove = new System.Windows.Forms.Button();
+            this.Clear = new System.Windows.Forms.Button();
+            this.CheckListBox = new System.Windows.Forms.CheckedListBox();
+            this.ListBox = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Control;
+            this.label1.Location = new System.Drawing.Point(327, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(376, 22);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "SIMPLE FAVORITE MOVIE PICKER APP";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.Control;
+            this.label2.Location = new System.Drawing.Point(325, 101);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(136, 19);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Movies/Series List";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.Control;
+            this.label3.Location = new System.Drawing.Point(521, 101);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(111, 19);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "My Movie List";
+            // 
+            // Add
+            // 
+            this.Add.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Add.Location = new System.Drawing.Point(381, 344);
+            this.Add.Name = "Add";
+            this.Add.Size = new System.Drawing.Size(94, 33);
+            this.Add.TabIndex = 3;
+            this.Add.Text = "Add";
+            this.Add.UseVisualStyleBackColor = true;
+            this.Add.Click += new System.EventHandler(this.Add_Click);
+            // 
+            // Remove
+            // 
+            this.Remove.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Remove.Location = new System.Drawing.Point(565, 344);
+            this.Remove.Name = "Remove";
+            this.Remove.Size = new System.Drawing.Size(93, 33);
+            this.Remove.TabIndex = 4;
+            this.Remove.Text = "Remove";
+            this.Remove.UseVisualStyleBackColor = true;
+            this.Remove.Click += new System.EventHandler(this.Remove_Click);
+            // 
+            // Clear
+            // 
+            this.Clear.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Clear.Location = new System.Drawing.Point(565, 383);
+            this.Clear.Name = "Clear";
+            this.Clear.Size = new System.Drawing.Size(93, 33);
+            this.Clear.TabIndex = 5;
+            this.Clear.Text = "Clear All";
+            this.Clear.UseVisualStyleBackColor = true;
+            this.Clear.Click += new System.EventHandler(this.Clear_Click);
+            // 
+            // CheckListBox
+            // 
+            this.CheckListBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.CheckListBox.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CheckListBox.FormattingEnabled = true;
+            this.CheckListBox.Location = new System.Drawing.Point(329, 123);
+            this.CheckListBox.Name = "CheckListBox";
+            this.CheckListBox.Size = new System.Drawing.Size(178, 202);
+            this.CheckListBox.TabIndex = 6;
+            this.CheckListBox.SelectedIndexChanged += new System.EventHandler(this.CheckListBox_SelectedIndexChanged);
+            // 
+            // ListBox
+            // 
+            this.ListBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.ListBox.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ListBox.FormattingEnabled = true;
+            this.ListBox.ItemHeight = 18;
+            this.ListBox.Location = new System.Drawing.Point(525, 123);
+            this.ListBox.Name = "ListBox";
+            this.ListBox.Size = new System.Drawing.Size(178, 200);
+            this.ListBox.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(730, 467);
+            this.Controls.Add(this.ListBox);
+            this.Controls.Add(this.CheckListBox);
+            this.Controls.Add(this.Clear);
+            this.Controls.Add(this.Remove);
+            this.Controls.Add(this.Add);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button Add;
+        private System.Windows.Forms.Button Remove;
+        private System.Windows.Forms.Button Clear;
+        private System.Windows.Forms.CheckedListBox CheckListBox;
+        private System.Windows.Forms.ListBox ListBox;
+    }
+}
+
---
+++ Chapter 1 Working with CheckedListBox - Part I/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SimpleFormsAppWithCheckedListBox
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            string[] Movies = { "Sonic", "Venom", "Stitch" };
+            CheckListBox.Items.AddRange(Movies);
+
+            CheckListBox.CheckOnClick = true;
+        }
+
+        private void CheckListBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Add_Click(object sender, EventArgs e)
+        {
+            foreach (var Movies in CheckListBox.Items) {
+                bool isSelected = CheckListBox.GetItemChecked(CheckListBox.Items.IndexOf(Movies));
+                if (isSelected && !ListBox.Items.Contains(Movies)) {
+                    ListBox.Items.Add(Movies);
+                }
+            }
+        }
+
+        private void Remove_Click(object sender, EventArgs e)
+        {
+            if (ListBox.Items.Count >= 1) {
+                ListBox.Items.Remove(ListBox.SelectedItem);
+            }
+        }
+
+        private void Clear_Click(object sender, EventArgs e)
+        {
+            ListBox.Items.Clear();
+        }
+    }
+}
