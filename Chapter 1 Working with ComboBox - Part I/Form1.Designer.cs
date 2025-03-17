3542fe686a72e2faf431a41fcbdfd103fd2d324e
Gian Kyrle Tradio <94472745+GianKyrleTradio@users.noreply.github.com>
2025-03-17 15:57:17 +0800

    Add files via upload

---
+++ Chapter 1 Working with ComboBox - Part I/Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace SimpleFormsAppWithComboBox
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.Select = new System.Windows.Forms.Button();
+            this.Add = new System.Windows.Forms.Button();
+            this.Remove = new System.Windows.Forms.Button();
+            this.Clear = new System.Windows.Forms.Button();
+            this.ComboBox = new System.Windows.Forms.ComboBox();
+            this.FoodCheckListBox = new System.Windows.Forms.CheckedListBox();
+            this.FoodListBox = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(28, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(366, 22);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "SIMPLE FAVORITE FOOD PICKER APP";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(28, 83);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 19);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Cuisine";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label3.Location = new System.Drawing.Point(184, 84);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(74, 19);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Food List";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(354, 83);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(159, 19);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "My Favorite Food List";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // Select
+            // 
+            this.Select.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.Select.Font = new System.Drawing.Font("Times New Roman", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Select.Location = new System.Drawing.Point(31, 138);
+            this.Select.Name = "Select";
+            this.Select.Size = new System.Drawing.Size(122, 23);
+            this.Select.TabIndex = 4;
+            this.Select.Text = "Select";
+            this.Select.UseVisualStyleBackColor = false;
+            this.Select.Click += new System.EventHandler(this.Select_Click);
+            // 
+            // Add
+            // 
+            this.Add.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.Add.Font = new System.Drawing.Font("Times New Roman", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Add.Location = new System.Drawing.Point(188, 281);
+            this.Add.Name = "Add";
+            this.Add.Size = new System.Drawing.Size(155, 23);
+            this.Add.TabIndex = 5;
+            this.Add.Text = "Add";
+            this.Add.UseVisualStyleBackColor = false;
+            this.Add.Click += new System.EventHandler(this.Add_Click);
+            // 
+            // Remove
+            // 
+            this.Remove.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.Remove.Font = new System.Drawing.Font("Times New Roman", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Remove.Location = new System.Drawing.Point(358, 281);
+            this.Remove.Name = "Remove";
+            this.Remove.Size = new System.Drawing.Size(155, 23);
+            this.Remove.TabIndex = 6;
+            this.Remove.Text = "Remove";
+            this.Remove.UseVisualStyleBackColor = false;
+            this.Remove.Click += new System.EventHandler(this.Remove_Click);
+            // 
+            // Clear
+            // 
+            this.Clear.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.Clear.Font = new System.Drawing.Font("Times New Roman", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Clear.Location = new System.Drawing.Point(358, 310);
+            this.Clear.Name = "Clear";
+            this.Clear.Size = new System.Drawing.Size(155, 23);
+            this.Clear.TabIndex = 7;
+            this.Clear.Text = "Clear All";
+            this.Clear.UseVisualStyleBackColor = false;
+            this.Clear.Click += new System.EventHandler(this.Clear_Click);
+            // 
+            // ComboBox
+            // 
+            this.ComboBox.FormattingEnabled = true;
+            this.ComboBox.Location = new System.Drawing.Point(31, 111);
+            this.ComboBox.Name = "ComboBox";
+            this.ComboBox.Size = new System.Drawing.Size(123, 21);
+            this.ComboBox.TabIndex = 8;
+            // 
+            // FoodCheckListBox
+            // 
+            this.FoodCheckListBox.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FoodCheckListBox.FormattingEnabled = true;
+            this.FoodCheckListBox.Location = new System.Drawing.Point(188, 106);
+            this.FoodCheckListBox.Name = "FoodCheckListBox";
+            this.FoodCheckListBox.Size = new System.Drawing.Size(155, 169);
+            this.FoodCheckListBox.TabIndex = 9;
+            this.FoodCheckListBox.SelectedIndexChanged += new System.EventHandler(this.FoodCheckListBox_SelectedIndexChanged);
+            // 
+            // FoodListBox
+            // 
+            this.FoodListBox.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FoodListBox.FormattingEnabled = true;
+            this.FoodListBox.ItemHeight = 15;
+            this.FoodListBox.Location = new System.Drawing.Point(358, 106);
+            this.FoodListBox.Name = "FoodListBox";
+            this.FoodListBox.Size = new System.Drawing.Size(155, 169);
+            this.FoodListBox.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(546, 357);
+            this.Controls.Add(this.FoodListBox);
+            this.Controls.Add(this.FoodCheckListBox);
+            this.Controls.Add(this.ComboBox);
+            this.Controls.Add(this.Clear);
+            this.Controls.Add(this.Remove);
+            this.Controls.Add(this.Add);
+            this.Controls.Add(this.Select);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button Select;
+        private System.Windows.Forms.Button Add;
+        private System.Windows.Forms.Button Remove;
+        private System.Windows.Forms.Button Clear;
+        private System.Windows.Forms.ComboBox ComboBox;
+        private System.Windows.Forms.CheckedListBox FoodCheckListBox;
+        private System.Windows.Forms.ListBox FoodListBox;
+    }
+}
+
---
+++ Chapter 1 Working with ComboBox - Part I/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SimpleFormsAppWithComboBox
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            string[] Cuisine = { "Filipino", "Japanese", "Chinese", "Korean" };
+            ComboBox.Items.AddRange(Cuisine);
+
+            ComboBox.DropDownStyle = ComboBoxStyle.DropDownList;
+
+            ComboBox.SelectedItem = 0;
+
+            FoodCheckListBox.CheckOnClick = true;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Add_Click(object sender, EventArgs e)
+        {
+            foreach (var item in FoodCheckListBox.Items){
+                bool isSelected = FoodCheckListBox.GetItemChecked(FoodCheckListBox.Items.IndexOf(item));
+                if (isSelected && !FoodListBox.Items.Contains(item))
+                {
+                    FoodListBox.Items.Add(item);
+                }
+            }
+        }
+
+        private void Remove_Click(object sender, EventArgs e)
+        {
+            if (FoodListBox.Items.Count >= 1)
+            {
+                FoodListBox.Items.Remove(FoodListBox.SelectedItem);
+            }
+        }
+
+        private void Clear_Click(object sender, EventArgs e)
+        {
+            FoodListBox.Items.Clear();
+        }
+
+        private void Select_Click(object sender, EventArgs e)
+        {
+            string[] FilipinoDish = { "Adobo", "Sinigang", "Lechon", "Pancit", "Lumpia" };
+            string[] JapaneseDish = { "Sushi", "Omurice", "Tonkatsu", "Yakitori", "Curry",};
+            string[] ChineseDish = { "Dim Sum", "Dumplings", "Tofu", "Char Siu", "Hot Pot" };
+            string[] KoreanDish = { "Kimchi", "Japchae", "Mandu", "Bulgog", "Bibimbap" };
+
+            FoodCheckListBox.Items.Clear();
+
+            if (ComboBox.SelectedItem.ToString().Contains("Filipino"))
+            {
+                FoodCheckListBox.Items.AddRange(FilipinoDish);
+            }
+            else if (ComboBox.SelectedItem.ToString().Contains("Japanese"))
+            {
+                FoodCheckListBox.Items.AddRange(JapaneseDish);
+            }
+            else if (ComboBox.SelectedItem.ToString().Contains("Chinese"))
+            {
+                FoodCheckListBox.Items.AddRange(ChineseDish);
+            }
+            else if (ComboBox.SelectedItem.ToString().Contains("Korean"))
+            {
+                FoodCheckListBox.Items.AddRange(KoreanDish);
+            }
+        }
+
+        private void FoodCheckListBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+    
+
